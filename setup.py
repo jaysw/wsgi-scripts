a3c9d1f393f0a50ec0b93c8671088b8d14c19059
Jay Sweeney <jay.sweeney@wotifgroup.com>
2012-07-25 15:31:14 +1000

    initial import

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='WsgiScripts',
+    version='0.0.1',
+    description="helpers for wrapping and running wsgi apps",
+    author='Jay Sweeney',
+    author_email='jay.sweeney@wotifgroup.com',
+    url='http://www.jaysweeney.com.au',
+    include_package_data=True,
+    zip_safe=False,
+    scripts=['scripts/tornado-run', 'scripts/flask-run'],
+    install_requires=['tornado']
+)
