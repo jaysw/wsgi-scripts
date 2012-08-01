685990c3fcff68c98c5a5215e91bcb108734b92e
Jay Sweeney <jay.sweeney@wotifgroup.com>
2012-08-01 15:19:52 +1000

    meh

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='WsgiScripts',
-    version='0.0.1',
+    version='0.0.2',
     description="helpers for wrapping and running wsgi apps",
     author='Jay Sweeney',
     author_email='jay.sweeney@wotifgroup.com',
